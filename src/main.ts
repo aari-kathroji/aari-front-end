c780fe9922f62398bd028488c0063e417970aad4
aari-kathroji <aari.kathroji@gmail.com>
2024-09-18 21:04:55 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
+
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
